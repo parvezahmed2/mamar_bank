71da3ce898d14c5f30c5b9f88d47177b0bfbb644
kiparvez <ndp943296@gmail.com>
2024-02-14 16:36:43 +0600

    django code

---
+++ transactions/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from .views import DepositMoneyView,  WithdrawMoneyView, LoanRequestView, LoanListView, PayLoanView, TransactionReportView
+
+
+urlpatterns = [
+    path('deposit/', DepositMoneyView.as_view(), name="deposit_money"),
+    path('report/', TransactionReportView.as_view(), name="transaction_report"),
+    path('withdraw/', WithdrawMoneyView.as_view(), name="withdraw_money"),
+    path('loan_request/', LoanRequestView.as_view(), name="loan_request"),
+    path('loans/', LoanListView.as_view(), name="loan_list"),
+   
+     
+    path('loan/<int:loan_id>/', PayLoanView.as_view(), name="loan_pay"),
+ 
+]
